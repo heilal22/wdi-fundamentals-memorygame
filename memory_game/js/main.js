30f3d4d13c1f3ffe7dc8c7ee8b887d58143bcdfe
heilal22 <35017964+heilal22@users.noreply.github.com>
2018-01-21 16:16:06 -0800

    pushing unit 11 logic game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,19 +23,39 @@ var cardsInPlay = [];
 
 //checking for match function
 var checkForMatch = function () {
-  if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");}
-  else {console.log("Sorry, try again.");}
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    alert("You found a match!");}
+    else {
+      console.log("Sorry, try again.");
+      alert("Sorry, try again.");}
   };
 
 //flipping card function
-  var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank);
+  var flipCard = function () {
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardIdImage);
-  console.log(cards[cardId].suit);
-  if (cardsInPlay.length === 2) {checkForMatch();}
+
+//show card image
+  this.setAttribute('src', cards[cardId].cardIdImage);
+  //two cards played?
+  if (cardsInPlay.length === 2) {checkForMatch();
+//reset
+cardsInPlay = [];
+  }
   };
+//create Board function
+var createBoard = function() {
+//loop
+for (var i = 0; i < cards.length; i++) {
+  //image element
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+}
+}
 
-
-flipCard(0);
-flipCard(2);
+createBoard();
